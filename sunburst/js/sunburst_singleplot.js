e95801ae220388ba88e7f46a6f7bb41d6fa018f0
Meg Pirrung <meganap@gmail.com>
2014-02-24 11:02:15 -0700

    Fixing color issue for sunburst

--- sunburst/js/sunburst_singleplot.js
+++ sunburst/js/sunburst_singleplot.js
@@ -1,8 +1,4 @@
-/* adapted from http://bl.ocks.org/kerryrodden/7090426 
-Copyright 2013 Google Inc. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+/* adapted from http://bl.ocks.org/kerryrodden/7090426 Copyright 2013 Google Inc. All Rights Reserved. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 and Mike Bostock's sunburst example on d3js.org, d3 example code Library released under BSD license. Copyright 2013 Mike Bostock.
 
