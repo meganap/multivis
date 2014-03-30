1e36e60dff932b63ca1c1c7c261f91187c691999
Daniel McDonald <mcdonadt@colorado.edu>
2014-03-30 10:17:40 -0600

    gets teh path

--- multivis/util.py
+++ multivis/util.py
@@ -9,3 +9,8 @@ def copy_support_files(file_path):
     """Copy support files to the output path"""
     support_files = os.path.join(PROJECT_DIR, 'support_files')
     copytree(support_files, file_path)
+
+
+def get_data_path(file_path):
+    """Return the data path relative to file_path"""
+    return os.path.join(file_path, 'data/multivis.biom')
