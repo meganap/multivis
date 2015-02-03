5987a989bb6b941bb77697d6cbad8ac513818c2b
Meg Pirrung <meganap@gmail.com>
2015-02-02 23:49:05 -0700

    Fixing unique ID generator so that it actually generates unique IDs

--- getNewID.php
+++ getNewID.php
@@ -13,7 +13,32 @@ if(! flock($current, LOCK_EX))
 $data = file_get_contents($file);
 $lines = preg_split('/[\n]/', $data);
 // create a new ID which is a hash of some values so that IDs do not intersect
-echo hash('adler32', sizeof($lines)+$_POST['url']+$date+rand(0, sizeof($lines)+50));
+echo v4();
 flock($current, LOCK_UN);
 fclose($current);
+
+/* from http://stackoverflow.com/questions/12270604/creating-collision-free-random-string */
+function v4()
+{
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+      // 32 bits for "time_low"
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+      // 16 bits for "time_mid"
+      mt_rand(0, 0xffff),
+
+      // 16 bits for "time_hi_and_version",
+      // four most significant bits holds version number 4
+      mt_rand(0, 0x0fff) | 0x4000,
+
+      // 16 bits, 8 bits for "clk_seq_hi_res",
+      // 8 bits for "clk_seq_low",
+      // two most significant bits holds zero and one for variant DCE1.1
+      mt_rand(0, 0x3fff) | 0x8000,
+
+      // 48 bits for "node"
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+}
 ?>
